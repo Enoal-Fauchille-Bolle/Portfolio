dd056cf60db2c72cac4975aa024bf2faf5dabb99
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2026-04-07 18:53:41 +0200

    chore(resume): link to external cv

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,11 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a
+              href="https://cv.enoal.fr"
+              className="resume-link"
+              target="_blank"
+              rel="noopener noreferrer">
               Curriculum Vitae
             </a>
           </nav>
