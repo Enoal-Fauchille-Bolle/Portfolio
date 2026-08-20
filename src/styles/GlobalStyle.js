eff7a45eee33ceb73e285ca89b61ba2c65159420
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2026-08-20 15:59:17 +0200

    chore(deps): drop prism and remark plugins left unused by pensieve removal

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import fonts from './fonts';
 import variables from './variables';
 import TransitionStyles from './TransitionStyles';
-import PrismStyles from './PrismStyles';
 
 const GlobalStyle = createGlobalStyle`
   ${fonts};
@@ -445,7 +444,6 @@ const GlobalStyle = createGlobalStyle`
 
   ${TransitionStyles};
 
-  ${PrismStyles};
 `;
 
 export default GlobalStyle;
